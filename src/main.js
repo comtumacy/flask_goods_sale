53a1e471f351c857e2d02004162163874e2147fa
1014741581@qq.com <1014741581@qq.com>
2019-11-07 00:39:47 +0800

    20191106

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入路由封装文件
+import router from '../route'
+// 导入axios包
+import axios from 'axios'
+// 导入MD5加密包
+import md5 from 'js-md5'
+// 按需导入Element UI组件库包
+import { Icon, Button } from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(Icon)
+Vue.use(Button)
 
 new Vue({
   render: h => h(App)
